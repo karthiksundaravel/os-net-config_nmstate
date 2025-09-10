448df2af0125724aeed5774e5578e1ff1d22fd2a
Karthik Sundaravel <ksundara@redhat.com>
2025-09-10 22:24:41 +0530

    ifcfg: provide methods to backup and remove ifcfg files
    - extended the  method remove_ifcfg_config() to remove all of ifcfg
      files associated with the device. eg: ifcfg-<dev>, route-<dev>,
      rule-<dev>
    - modified move_ifcfg() to backup all the ifcfg files of the device
    
    Signed-off-by: Karthik Sundaravel <ksundara@redhat.com>

--- os_net_config/impl_ifcfg.py
+++ os_net_config/impl_ifcfg.py
@@ -46,7 +46,7 @@ _ROUTE_TABLE_DEFAULT = """# reserved values
 #
 #1\tinr.ruhep\n"""
 
-PURGE_IFCFG_FILES = '/var/lib/os-net-config/ifcfg-purge/'
+BACKUP_IFCFG_FILES_PATH = '/var/lib/os-net-config/ifcfg-files/'
 NETWORK_SCRIPTS_PATH = '/etc/sysconfig/network-scripts/'
 
 
@@ -57,9 +57,22 @@ def ifcfg_config_path(name):
 def remove_ifcfg_config(ifname):
     if re.match(r'[\w-]+$', ifname):
         ifcfg_file = ifcfg_config_path(ifname)
-        if os.path.exists(ifcfg_file):
-            logger.info("removing existing ifcfg script for intf: %s", ifname)
-            os.remove(ifcfg_file)
+        route_file = route_config_path(ifname)
+        route6_file = route6_config_path(ifname)
+        rule_file = route_rule_config_path(ifname)
+
+        src_files = [
+            ifcfg_file,
+            route_file,
+            route6_file,
+            rule_file
+        ]
+        for src in src_files:
+            if os.path.exists(src):
+                logger.info("%s: removing file %s", ifname, src)
+                os.remove(src)
+            else:
+                logger.warning("%s: not found", src)
 
 
 # NOTE(dprince): added here for testability
@@ -2390,34 +2403,37 @@ class IfcfgNetConfig(os_net_config.NetConfig):
 
         return update_files
 
-    def move_ifcfg(self, iface_name):
+    def backup_ifcfg(self, iface_name, backup_path, force=False):
         ifcfg_file = ifcfg_config_path(iface_name)
         route_file = route_config_path(iface_name)
         route6_file = route6_config_path(iface_name)
         rule_file = route_rule_config_path(iface_name)
 
-        if not os.path.exists(PURGE_IFCFG_FILES):
-            os.makedirs(PURGE_IFCFG_FILES)
+        if not os.path.exists(backup_path):
+            os.makedirs(backup_path)
+        src_files = [
+            ifcfg_file,
+            route_file,
+            route6_file,
+            rule_file
+        ]
+        if self._is_os_net_config_managed(ifcfg_file) or force:
+            for src in src_files:
+                if os.path.exists(src):
+                    filename = os.path.basename(src)
+                    bkup_file = os.path.join(backup_path, filename)
+                    logger.info("Copying %s -> %s", src, bkup_file)
+                    shutil.copy(src, bkup_file)
+                else:
+                    logger.warning("%s: not found", src)
+
+    def _is_os_net_config_managed(self, ifcfg_file):
         if os.path.exists(ifcfg_file):
-            new_ifcfg_file = os.path.join(PURGE_IFCFG_FILES,
-                                          f'ifcfg-{iface_name}')
-            logger.info("Moving %s -> %s", ifcfg_file, new_ifcfg_file)
-            shutil.move(ifcfg_file, new_ifcfg_file)
-        if os.path.exists(route_file):
-            new_route_file = os.path.join(PURGE_IFCFG_FILES,
-                                          f'route-{iface_name}')
-            logger.info("Moving %s -> %s", route_file, new_route_file)
-            shutil.move(route_file, new_route_file)
-        if os.path.exists(route6_file):
-            new_route6_file = os.path.join(PURGE_IFCFG_FILES,
-                                           f'route6-{iface_name}')
-            logger.info("Moving %s - %s", route6_file, new_route6_file)
-            shutil.move(route6_file, new_route6_file)
-        if os.path.exists(rule_file):
-            new_rule_file = os.path.join(PURGE_IFCFG_FILES,
-                                         f'rule-{iface_name}')
-            logger.info("Moving %s -> %s", rule_file, new_rule_file)
-            shutil.move(rule_file, new_rule_file)
+            with open(ifcfg_file, 'r') as f:
+                file_content = f.read()
+            if _IFCFG_FILE_HEADER in file_content:
+                return True
+        return False
 
     def roll_back_migration(self):
         logger.info('Rolling back to ifcfg provider')
@@ -2430,12 +2446,12 @@ class IfcfgNetConfig(os_net_config.NetConfig):
 
     def _restore_ifcfg_files(self):
         logger.info('Restoring the ifcfg files')
-        for file in os.listdir(PURGE_IFCFG_FILES):
+        for file in os.listdir(BACKUP_IFCFG_FILES_PATH):
             if file.startswith('ifcfg-') or \
                 file.startswith('rule-') or \
                 file.startswith('route-') or \
                 file.startswith('route6-'):
-                file_path = os.path.join(PURGE_IFCFG_FILES, file)
+                file_path = os.path.join(BACKUP_IFCFG_FILES_PATH, file)
                 new_file_path = os.path.join(NETWORK_SCRIPTS_PATH, file)
                 try:
                     shutil.copy(file_path, new_file_path)
@@ -2469,7 +2485,6 @@ class IfcfgNetConfig(os_net_config.NetConfig):
             if _IFCFG_FILE_HEADER in file_content:
                 logger.info("%s: Bringing down", iface_name)
                 self.ifdown(iface_name)
-                self.move_ifcfg(iface_name)
             else:
                 logger.info(
                     "%s: Device is not managed by ifcfg provider", iface_name
